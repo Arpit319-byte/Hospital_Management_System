f450c16568ac6239a9ef8bc59872794814ccf689
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-10-19 14:27:39 +0530

    Added Attributes for Patient Class

--- src/main/java/com/example/Hospital_Management_System/model/Patient.java
+++ src/main/java/com/example/Hospital_Management_System/model/Patient.java
@@ -1,4 +1,36 @@
 package com.example.Hospital_Management_System.model;
 
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Patient {
+
+
+    private int id;
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String gender;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Patient(String name,String address,String phoneNumber,String gender,String email){
+        this.name =name;
+        this.address=address;
+        this.phoneNumber=phoneNumber;
+        this.gender=gender;
+        this.email=email;
+    }
+
+
 }
