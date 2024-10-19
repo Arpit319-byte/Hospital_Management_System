e354c5f093052711332949e1cae50d37e8001f51
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-10-19 15:40:01 +0530

    Added the attributes for Appoitment Class

--- src/main/java/com/example/Hospital_Management_System/model/Appointment.java
+++ src/main/java/com/example/Hospital_Management_System/model/Appointment.java
@@ -1,4 +1,24 @@
 package com.example.Hospital_Management_System.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Appointment {
+
+    private int id;
+    private int patientId;
+    private int doctorId;
+    private Date appointmentDate;
+    private status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
