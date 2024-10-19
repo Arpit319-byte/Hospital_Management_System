b87e47dc21806bf4a5f2566605c3b105d84582ee
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-10-19 14:46:06 +0530

    Added attributes for Doctor class

--- src/main/java/com/example/Hospital_Management_System/model/Doctor.java
+++ src/main/java/com/example/Hospital_Management_System/model/Doctor.java
@@ -1,4 +1,20 @@
 package com.example.Hospital_Management_System.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
 public class Doctor {
+
+    private int id;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String experience;
+    private Timestamp createdAt;
+    private Timestamp updateAt;
+
 }
