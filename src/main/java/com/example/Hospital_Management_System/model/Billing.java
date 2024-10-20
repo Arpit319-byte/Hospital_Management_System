c554da74704ac85f75fe1799dbb61849012de7fa
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-10-20 20:02:12 +0530

    Added the attributes for Billing class

--- src/main/java/com/example/Hospital_Management_System/model/Billing.java
+++ src/main/java/com/example/Hospital_Management_System/model/Billing.java
@@ -1,4 +1,26 @@
 package com.example.Hospital_Management_System.model;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Billing {
+
+    private int id;
+    private int patientId;
+    private String patientName;
+    private int doctorId;
+    private String doctorName;
+    private double amount;
+    private Status status;
+    private PaymentMethod paymentMethod;
+    private LocalDateTime billingDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
