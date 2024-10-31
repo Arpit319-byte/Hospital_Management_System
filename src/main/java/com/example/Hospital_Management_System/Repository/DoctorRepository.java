8fc5d3bb70f715b2ba87558ce7ae6ccf0adf7891
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-10-31 19:01:42 +0530

    Added the Repository Annotation for all repository classes

--- src/main/java/com/example/Hospital_Management_System/Repository/DoctorRepository.java
+++ src/main/java/com/example/Hospital_Management_System/Repository/DoctorRepository.java
@@ -2,6 +2,8 @@ package com.example.Hospital_Management_System.Repository;
 
 import com.example.Hospital_Management_System.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DoctorRepository extends JpaRepository<Doctor,Integer> {
 }
--- src/main/java/com/example/Hospital_Management_System/Repository/PatientRepository.java
+++ src/main/java/com/example/Hospital_Management_System/Repository/PatientRepository.java
@@ -1,6 +1,8 @@
 package com.example.Hospital_Management_System.Repository;
 import com.example.Hospital_Management_System.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 }
