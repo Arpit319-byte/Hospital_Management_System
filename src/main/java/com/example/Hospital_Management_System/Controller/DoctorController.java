933e2fba254d251bce3c0971500c13b4710f99cf
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-10-31 17:57:25 +0530

    Added the CRUD operations in the Doctor Controller class

--- src/main/java/com/example/Hospital_Management_System/Controller/DoctorController.java
+++ src/main/java/com/example/Hospital_Management_System/Controller/DoctorController.java
@@ -1,4 +1,63 @@
 package com.example.Hospital_Management_System.Controller;
 
+import com.example.Hospital_Management_System.Service.DoctorService;
+import com.example.Hospital_Management_System.model.Doctor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/doctor")
 public class DoctorController {
+
+    private final DoctorService doctorService;
+
+    @Autowired
+    public DoctorController(DoctorService doctorService){
+        this.doctorService=doctorService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Doctor>> getAllDoctor(){
+        return new ResponseEntity<List<Doctor>>(doctorService.getAllDoctor(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{doctorId}")
+    public ResponseEntity<Doctor> getDoctorById(@PathVariable int doctorId){
+        Doctor d=doctorService.getDoctorById(doctorId);
+
+        if(d != null)
+            return new ResponseEntity<>(d,HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Doctor> addDoctor(@RequestBody Doctor doctor){
+
+    }
+
+    @DeleteMapping("/{doctorId}")
+    public ResponseEntity<Void> deleteDoctorById(@PathVariable int doctorId){
+
+        boolean isRemoved=doctorService.deleteDoctorById();
+
+        if(!isRemoved)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("{/{doctorId}")
+    public ResponseEntity<Doctor> updateDoctorById(){
+           Doctor d=doctorService.updateDoctorId();
+
+           if(d == null)
+               return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+           return new ResponseEntity<>(d,HttpStatus.OK);
+
+    }
+
 }
